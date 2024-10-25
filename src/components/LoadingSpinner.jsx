ae04e7dd68e46a157c067a9c5165fce8ba1f56ca
lgb94 <lgb_94@hotmail.com>
2024-10-25 18:20:26 +0100

    fixed filepath on loading spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,7 +1,9 @@
+import spinnerIcon from "../../assets/spinner-icon.png"
+
 const LoadingSpinner = () => {
     return (
         <div className="spinner-wrapper">
-            <img className="spinner" src="assets/spinner-icon.png" alt="A statue of a woman in an energetic pose" />
+            <img className="spinner" src={spinnerIcon} alt="A statue of a woman in an energetic pose" />
             <p>Page loading...</p>
         </div>
     )
